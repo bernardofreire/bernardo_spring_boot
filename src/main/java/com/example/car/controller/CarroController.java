893fd91b72ebde49c98befaa05f28182ba9a308b
Bernardo Freire <b.bernardo9815@gmail.com>
2025-06-25 15:39:32 -0300

    atualizacao do app.prop 2

--- src/main/java/com/example/car/controller/CarroController.java
+++ src/main/java/com/example/car/controller/CarroController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/carros")
 @Tag(name = "Carros", description = "Operações relacionadas a veículos")
--- src/main/java/com/example/car/controller/MarcaController.java
+++ src/main/java/com/example/car/controller/MarcaController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+@CrossOrigin(origins = "*")
 @RestController
 @RequestMapping("/marcas")
 @Tag(name = "Marcas", description = "Operações relacionadas às marcas de carros")
